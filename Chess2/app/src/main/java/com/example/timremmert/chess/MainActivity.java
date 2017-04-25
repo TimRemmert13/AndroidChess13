e939f9294c7ce404c053f4713ab05f18ca53a9cd
timremmert <tbr23@scarletmail.rutgers.edu>
2017-04-25 15:20:05 -0400

    test

---
+++ Chess2/app/src/main/java/com/example/timremmert/chess/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.timremmert.chess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//commit
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
